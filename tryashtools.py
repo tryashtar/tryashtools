e39eb33bbb26eedb44ffdf500fd6f5662da38c3e
tryashtar <tryashtar@gmail.com>
2024-09-04 17:16:47 -0600

    Better typings

--- tryashtools.py
+++ tryashtools.py
@@ -3,33 +3,33 @@ import json
 import shutil
 import yaml
 
-def setup_path(file : str):
+def setup_path(file: os.PathLike | str):
    folder = os.path.dirname(file)
    if folder != "" and not os.path.isdir(folder):
       os.makedirs(folder)
 
-def delete_folder(folder : str):
+def delete_folder(folder: os.PathLike | str):
    if os.path.isdir(folder):
       shutil.rmtree(folder)
 
-def delete_file(file : str):
+def delete_file(file: os.PathLike | str):
    if os.path.exists(file):
       os.remove(file)
 
-def copy_file(filefrom : str, fileto : str):
+def copy_file(filefrom: os.PathLike | str, fileto: os.PathLike | str):
    setup_path(fileto)
    shutil.copyfile(filefrom, fileto)
 
-def move_file(filefrom : str, fileto : str):
+def move_file(filefrom: os.PathLike | str, fileto: os.PathLike | str):
    setup_path(fileto)
    shutil.move(filefrom, fileto)
 
-def write_yaml(y, path : str):
+def write_yaml(y, path: os.PathLike | str):
    setup_path(path)
    with open(path, "w", encoding="utf-8") as file:
       yaml.dump(y, file, default_flow_style=False, sort_keys=False, allow_unicode=True)
 
-def write_json(j, path : str, mini=False):
+def write_json(j, path: os.PathLike | str, mini=False):
    setup_path(path)
    with open(path, "w", encoding="utf8") as file:
       if mini:
@@ -37,47 +37,52 @@ def write_json(j, path : str, mini=False):
       else:
          json.dump(j, file, indent=3)
 
-def write_lines(lines : list[str], path : str):
+def write_lines(lines: list[str], path: os.PathLike | str):
    setup_path(path)
    with open(path, "w", encoding="utf8") as file:
       for line in lines:
          file.write(line+"\n")
 
-def write_text(text : str, path : str):
+def write_bytes(data: bytes, path: os.PathLike | str):
+   setup_path(path)
+   with open(path, "wb") as file:
+      file.write(data)
+
+def write_text(text: str, path: os.PathLike | str):
    setup_path(path)
    with open(path, "w", encoding="utf8") as file:
       file.write(text)
 
-def write_lang(pairs : dict[str,str], path : str):
+def write_lang(pairs: dict[str,str], path: os.PathLike | str):
    setup_path(path)
    with open(path, "w", encoding="utf8") as file:
       for k,v in pairs.items():
          file.write(f"{k}={v}\n")
 
-def read_yaml(path : str):
+def read_yaml(path: os.PathLike | str):
    with open(path, "r", encoding="utf-8") as file:
       return yaml.load(file, yaml.CLoader)
 
-def read_json(path : str):
+def read_json(path: os.PathLike | str):
    with open(path, "r", encoding="utf-8") as file:
       return json.loads(file.read())
 
-def read_lines(path : str):
+def read_lines(path: os.PathLike | str):
    with open(path, "r", encoding="utf8") as file:
       return [line.rstrip('\n') for line in file]
 
-def read_text(path : str):
+def read_text(path: os.PathLike | str):
    with open(path, "r", encoding="utf8") as file:
       return file.read()
 
-def get_items(folder : str, recursive=False):
+def get_items(folder: os.PathLike | str, recursive=False):
    for file in os.listdir(folder):
       full = os.path.abspath(os.path.join(folder, file))
       if recursive and os.path.isdir(full):
          yield from get_items(full, True)
       yield full
 
-def get_files(folder : str, recursive=False):
+def get_files(folder: os.PathLike | str, recursive=False):
    for file in os.listdir(folder):
       full = os.path.abspath(os.path.join(folder, file))
       if os.path.isfile(full):
@@ -85,7 +90,7 @@ def get_files(folder : str, recursive=False):
       elif recursive:
          yield from get_files(full, True)
 
-def get_folders(folder : str, recursive=False):
+def get_folders(folder: os.PathLike | str, recursive=False):
    for file in os.listdir(folder):
       full = os.path.abspath(os.path.join(folder, file))
       if os.path.isdir(full):
@@ -93,16 +98,16 @@ def get_folders(folder : str, recursive=False):
          if recursive:
             yield from get_folders(full, True)
 
-def base_name(filename : str):
+def base_name(filename: os.PathLike | str):
    return remove_extension(os.path.basename(filename))
 
-def extension(filename : str):
+def extension(filename: os.PathLike | str) -> str:
    return os.path.splitext(filename)[1]
 
-def remove_extension(filename : str):
+def remove_extension(filename: os.PathLike | str) -> str:
    return os.path.splitext(filename)[0]
 
-def path_list(filename : str):
+def path_list(filename: os.PathLike | str):
    first, second = os.path.split(filename)
    if first == "":
       yield second
